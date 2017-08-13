c95a876fcfabbeae60541117de442761b26647ca
OliviaVG <oliviavangerrevink@gmail.com>
2017-08-13 18:31:22 +0200

    SassHomeworkV1

---
+++ FE1011/Sass/ClassAsFA/compile.cs
@@ -0,0 +1,34 @@
+body {
+  margin: 0 auto;
+  text-position: center;
+  background: url("Images/Leonardo-da-Vinci background1 1920x1200.jpg");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  background-color: #191919; }
+
+header h1 {
+  background-color: black;
+  color: white;
+  text-align: center;
+  font-family: "Samplino";
+  src: url("fonts/Samplino.eot?") format("eot"), url("fonts/Samplino.woff2") format("woff2"), url("fonts/Samplino.woff") format("woff"), url("fonts/Samplino.ttf") format("truetype"), url("fonts/Samplino.svg#Samplino") format("svg"); }
+  header h1 span {
+    text-align: left; }
+  header h1 button {
+    text-align: right; }
+  header h1 h1:hover {
+    background-color: pink; }
+  header h1 h1 {
+    transition: background-color 4s linear; }
+
+section .text1, section .text2 {
+  background-color: #191919;
+  color: #e5e5e5;
+  font-family: "Samplino";
+  src: url("fonts/Samplino.eot?") format("eot"), url("fonts/Samplino.woff2") format("woff2"), url("fonts/Samplino.woff") format("woff"), url("fonts/Samplino.ttf") format("truetype"), url("fonts/Samplino.svg#Samplino") format("svg"); }
+
+footer {
+  position: relative; }
+  footer span {
+    font-size: 25px; }
